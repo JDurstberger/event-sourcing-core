9dc0fc2c821fe9f58e55f1bec71035efca934632
Jakob Durstberger <jakob@durstberger.me>
2023-01-05 07:35:42 +0100

    Fix variable assignment in install-clojure script.

--- scripts/ci/common/install-clojure.sh
+++ scripts/ci/common/install-clojure.sh
@@ -4,7 +4,7 @@
 set -e
 set -o pipefail
 
-$CLOJURE_VERSION=1.11.1.1208
+CLOJURE_VERSION=1.11.1.1208
 
 curl -O https://download.clojure.org/install/linux-install-${CLOJURE_VERSION}.sh
 chmod +x linux-install-${CLOJURE_VERSION}.sh
