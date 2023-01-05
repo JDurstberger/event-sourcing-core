82312b55aef8d7c61d08fc520d9f208c7d49900f
Jakob Durstberger <jakob@durstberger.me>
2023-01-05 07:36:59 +0100

    Don't run clojure install as sudo.

--- scripts/ci/common/install-clojure.sh
+++ scripts/ci/common/install-clojure.sh
@@ -8,4 +8,4 @@ CLOJURE_VERSION=1.11.1.1208
 
 curl -O https://download.clojure.org/install/linux-install-${CLOJURE_VERSION}.sh
 chmod +x linux-install-${CLOJURE_VERSION}.sh
-sudo ./linux-install-${CLOJURE_VERSION}.sh
+./linux-install-${CLOJURE_VERSION}.sh
